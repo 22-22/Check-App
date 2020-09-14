428d35dd865c4c2462a3408eeb555d4d0143e3c3
NastiaKoval <anasteziyam@gmail.com>
2020-09-14 17:37:49 +0300

    feat: add ant design

--- src/components/Review-request/Review-request.js
+++ src/components/Review-request/Review-request.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { Modal, Form, Input, Button, Select } from 'antd';
+import "./reviewRequest.css"
+
+const { Option } = Select;
+
+const layout = {
+    labelCol: {
+      span: 6,
+    },
+    wrapperCol: {
+      span: 18,
+    },
+};
 
 class ReviewRequest extends React.Component {
 
@@ -8,16 +21,19 @@ class ReviewRequest extends React.Component {
             task: this.props.tasks[0],
             demo: '',
             PR: '',
+            visible: true,
         }
     }
 
+    formRef = React.createRef();
+
     onSubmit = (event) => {
         console.log(this.state);
-        event.preventDefault();
+        this.setState({ visible: false });
     }
 
-    onTaskSelect = (event) => {
-        this.setState({task: event.target.value});
+    onTaskSelect = (value) => {
+        this.setState({task: value});
     }
 
     onChangeDemo = (event) => {
@@ -27,23 +43,53 @@ class ReviewRequest extends React.Component {
     onChangePR = (event) => {
         this.setState({PR: event.target.value});
     }
+
+    onReset = () => {
+        this.formRef.current.resetFields();
+    };
+
+    handleCancel = () => {
+        this.setState({ visible: false });
+    };
+
+    
     render() {
         return (
             <div className="review-request-form">
-                <form onSubmit={this.onSubmit}>
-                    <label> 
-                        Task: 
-                        <select name="task" value={this.state.task} onChange={this.onTaskSelect}>
-                            {this.props.tasks.map(task => <option value={task} key={task}>{task}</option>)}
-                        </select>
-                    </label>
-                    <p><label> Demo: <input type="text" name="demo-link" value={this.state.demo} placeholder="add link to your demo"
-                           onChange={this.onChangeDemo}/></label></p>
-                    <p><label> Pull Request: <input type="text" name="PR-link" value={this.state.PR} placeholder="add link to your pull request"
-                           onChange={this.onChangePR}/></label></p>
-                    <p><input type="submit" value="make a self check" /></p>
-                    <p><input type="submit" value="later" /></p>
-                </form>
+                <Modal
+                    title="Make a review request"
+                    visible={this.state.visible}
+                    footer={null}
+                    onCancel={this.handleCancel}
+                >
+                    <Form {...layout} ref={this.formRef} onFinish={this.onSubmit}>
+                        <Form.Item name="task" value={this.state.task} label="Task:" rules={[{ required: true }]}>
+                            <Select
+                            placeholder="Select a task"
+                            onChange={this.onTaskSelect}
+                            >
+                                {this.props.tasks.map(task => <Option value={task} key={task}>{task}</Option>)}
+                            </Select>
+                        </Form.Item>
+                        <Form.Item name="demo-link" value={this.state.demo} label="Demo:" rules={[{ required: true }]}>
+                            <Input type="text" placeholder="add link to your demo"
+                            onChange={this.onChangeDemo}/>
+                        </Form.Item>
+                        <Form.Item name="PR-link" value={this.state.PR} label="Pull Request:" rules={[{ required: true }]}>
+                            <Input type="text" placeholder="add link to your pull request"
+                            onChange={this.onChangePR}/>
+                        </Form.Item>
+                        <Form.Item className="review-request-btn-block">
+                            <Button type="primary" htmlType="submit" className="review-request-btn">
+                                Make a self check
+                            </Button>
+                            <Button htmlType="button" onClick={this.onReset}>
+                                Reset
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+                
             </div>
         )
     }
