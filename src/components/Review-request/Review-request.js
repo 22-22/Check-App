fcab0faa1df5011a23f501bf217ee833bb2f2bcd
NastiaKoval <anasteziyam@gmail.com>
2020-09-17 15:32:37 +0300

    feat: change component to functionl

--- src/components/Review-request/Review-request.js
+++ src/components/Review-request/Review-request.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { Modal, Form, Input, Button, Select } from 'antd';
+import { useDispatch, useSelector } from "react-redux";
+import { getActiveTasks } from '../../redux/actions/reviewRequest';
+import checkAuth from "../../utils/checkAuth";
+
 import "./reviewRequest.css"
+import { Form, Input, Button, Select } from 'antd';
 
 const { Option } = Select;
 
@@ -9,90 +13,70 @@ const layout = {
       span: 6,
     },
     wrapperCol: {
-      span: 18,
+      span: 12,
     },
 };
 
-class ReviewRequest extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            task: this.props.tasks[0],
-            demo: '',
-            PR: '',
-            visible: true,
-        }
-    }
+function ReviewRequest({ history }) {
+    const [status, setStatus] = React.useState("published");
+    const dispatch = useDispatch();
 
-    formRef = React.createRef();
+    const { authentication, infoUser } = useSelector(({ statesAccount }) => statesAccount);
+     
+    const { activeTasks } = useSelector(({ reviewRequest }) => reviewRequest);
+    
+    React.useEffect(() => {
+        !authentication && checkAuth(history, authentication, dispatch, "/review-request");
+        dispatch(getActiveTasks(status));
+    }, [status]);
 
-    onSubmit = (event) => {
+    
+    console.log(activeTasks)
+    const [task, setTask] = React.useState("");
+    const [demo, setDemo] = React.useState("");
+    const [PR, setPR] = React.useState("");
+
+    const [form] = Form.useForm();
+
+    const onSubmit = (event) => {
         console.log(this.state);
-        this.setState({ visible: false });
     }
 
-    onTaskSelect = (value) => {
-        this.setState({task: value});
-    }
-
-    onChangeDemo = (event) => {
-        this.setState({demo: event.target.value});
-    }
-
-    onChangePR = (event) => {
-        this.setState({PR: event.target.value});
-    }
-
-    onReset = () => {
+    const onReset = () => {
         this.formRef.current.resetFields();
     };
 
-    handleCancel = () => {
-        this.setState({ visible: false });
-    };
-
-    
-    render() {
-        return (
-            <div className="review-request-form">
-                <Modal
-                    title="Make a review request"
-                    visible={this.state.visible}
-                    footer={null}
-                    onCancel={this.handleCancel}
-                >
-                    <Form {...layout} ref={this.formRef} onFinish={this.onSubmit}>
-                        <Form.Item name="task" value={this.state.task} label="Task:" rules={[{ required: true }]}>
-                            <Select
-                            placeholder="Select a task"
-                            onChange={this.onTaskSelect}
-                            >
-                                {this.props.tasks.map(task => <Option value={task} key={task}>{task}</Option>)}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item name="demo-link" value={this.state.demo} label="Demo:" rules={[{ required: true }]}>
-                            <Input type="text" placeholder="add link to your demo"
-                            onChange={this.onChangeDemo}/>
-                        </Form.Item>
-                        <Form.Item name="PR-link" value={this.state.PR} label="Pull Request:" rules={[{ required: true }]}>
-                            <Input type="text" placeholder="add link to your pull request"
-                            onChange={this.onChangePR}/>
-                        </Form.Item>
-                        <Form.Item className="review-request-btn-block">
-                            <Button type="primary" htmlType="submit" className="review-request-btn">
-                                Make a self check
-                            </Button>
-                            <Button htmlType="button" onClick={this.onReset}>
-                                Reset
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </Modal>
-                
-            </div>
-        )
-    }
+    return (
+        <div className="review-request-form">
+            <Form {...layout} form={form} onFinish={onSubmit}>
+                <Form.Item name="task" value={task} label="Task:" rules={[{ required: true }]}>
+                    <Select
+                    placeholder="Select a task"
+                    onChange={value => setTask(value)}
+                    >
+                        {activeTasks ? activeTasks.map(task => <Option value={task.title} key={task.title}>{task.title}</Option>) : ''}
+                    </Select>
+                </Form.Item>
+                <Form.Item name="demo-link" value={demo} label="Demo:" rules={[{ required: true }]}>
+                    <Input type="text" placeholder="add link to your demo"
+                    onChange={e => setDemo(e.target.value)}/>
+                </Form.Item>
+                <Form.Item name="PR-link" value={PR} label="Pull Request:" rules={[{ required: true }]}>
+                    <Input type="text" placeholder="add link to your pull request"
+                    onChange={e => setPR(e.target.value)}/>
+                </Form.Item>
+                <Form.Item className="review-request-btn-block">
+                    <Button htmlType="button" onClick={onReset}>
+                        Reset
+                    </Button>
+                    <Button type="primary" htmlType="submit" className="review-request-btn">
+                        Make a self check
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    )
 }
 
 export default ReviewRequest;
