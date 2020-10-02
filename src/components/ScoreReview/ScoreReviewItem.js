10dc57c6bff14e58573d8252342a6e559f8b6eef
22-22 <47010136+22-22@users.noreply.github.com>
2020-10-02 20:07:55 +0300

    Fix: update score review

--- src/components/ScoreReview/ScoreReviewItem.js
+++ src/components/ScoreReview/ScoreReviewItem.js
@@ -6,10 +6,10 @@ function ScoreReviewItem(props) {
         <div className="score-review-item">
             <div className="score-review-item-description-comment">
                 <p className="score-review-item-description">{props.itemObj.description}</p>
-                {props.marks[props.category] ? props.marks[props.category][props.itemI]["comment"] ? <p className="score-review-item-comment">comment: {props.marks[props.category][props.itemI]["comment"]}</p> : "" : ""}
+                {props.marks[props.category][props.itemI] ? props.marks[props.category][props.itemI]["comment"] ? <p className="score-review-item-comment">comment: {props.marks[props.category][props.itemI]["comment"]}</p> : "" : ""}
             </div>
             
-            {props.marks[props.category] ? <p className="score-review-marks">{`${props.marks[props.category][props.itemI]["score"]}/${props.itemObj.maxScore}`}</p> : "Checked by a mentor only"}
+            {props.marks[props.category][props.itemI] ? <p className="score-review-marks">{`${props.marks[props.category][props.itemI]["score"]}/${props.itemObj.maxScore}`}</p> : "Checked by a mentor only"}
         </div>
     )
 }
